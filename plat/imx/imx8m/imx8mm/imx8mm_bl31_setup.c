6d63e244517db854b63e368bea7bf2985aed1c1a
Nate Drude <nate.d@variscite.com>
2020-08-20 10:38:07 -0500

    imx8m: Disable ATF console_imx_uart_register for compatibility with all Variscite imx8m SOMs
    Signed-off-by: Nate Drude <nate.d@variscite.com>
    (cherry picked from commit c973b7acfeee23e319077947199416e8b7f3bc73)

--- plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
+++ plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -189,7 +189,9 @@ static void bl31_tzc380_setup(void)
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		u_register_t arg2, u_register_t arg3)
 {
+#if VAR_ENABLE_IMX_UART
 	static console_t console;
+#endif
 	int i;
 
 	/* Enable CSU NS access permission */
@@ -203,10 +205,12 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 
 	imx_csu_init(csu_cfg);
 
+#if VAR_ENABLE_IMX_UART
 	console_imx_uart_register(IMX_BOOT_UART_BASE, IMX_BOOT_UART_CLK_IN_HZ,
 		IMX_CONSOLE_BAUDRATE, &console);
 	/* This console is only used for boot stage */
 	console_set_scope(&console, CONSOLE_FLAG_BOOT);
+#endif
 
 	imx8m_caam_init();
 
