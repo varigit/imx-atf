e4dd94cd7e586ccb40cb6ab52842dd4fcd0aaa80
Nate Drude <nate.d@variscite.com>
2020-08-28 13:07:29 -0500

    imx8mm: Remove uart2 and uart4 domain restrictions
    i.MX8MM ATF code assigns UART2 to the A53 Domain and UART4 to the M4 domain.
    Remove these assignments to allow for usage in either domain.
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
+++ plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -57,10 +57,6 @@ static const struct imx_rdc_cfg rdc[] = {
 	/* Master domain assignment */
 	RDC_MDAn(RDC_MDA_M4, DID1),
 
-	/* peripherals domain permission */
-	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
-	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
-
 	/* memory region */
 
 	/* Sentinel */
