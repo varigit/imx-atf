varigit/imx-atf

commit 1997e363065ad5801ab43cae120ceb706995e86d

plat: imx8m: Disable ATF console_imx_uart_register
Disable ATF console_imx_uart_register for compatibility with all
Variscite imx8m SOMs

Signed-off-by: Nate Drude <nate.d@variscite.com>
(cherry picked from commit 32f29e5cb30102e30476a34b00238f9523b8e3ac)

plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -190,8 +190,9 @@ static void bl31_tzc380_setup(void)
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		u_register_t arg2, u_register_t arg3)
 {
+#if VAR_ENABLE_IMX_UART
 	unsigned int console_base = IMX_BOOT_UART_BASE;
-	static console_t console;
+#endif
 	int i;
 
 	/* Enable CSU NS access permission */
@@ -205,6 +206,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 
 	imx_csu_init(csu_cfg);
 
+#if VAR_ENABLE_IMX_UART
 	if (console_base == 0U) {
 		console_base = imx8m_uart_get_base();
 	}
@@ -213,6 +215,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		IMX_CONSOLE_BAUDRATE, &console);
 	/* This console is only used for boot stage */
 	console_set_scope(&console, CONSOLE_FLAG_BOOT);
+#endif
 
 	imx8m_caam_init();
 

plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
@@ -185,8 +185,9 @@ static void bl31_tzc380_setup(void)
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		u_register_t arg2, u_register_t arg3)
 {
+#if VAR_ENABLE_IMX_UART
 	unsigned int console_base = IMX_BOOT_UART_BASE;
-	static console_t console;
+#endif
 	unsigned int val;
 	int i;
 
@@ -213,6 +214,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 	val = mmio_read_32(IMX_IOMUX_GPR_BASE + 0x2c);
 	mmio_write_32(IMX_IOMUX_GPR_BASE + 0x2c, val | 0x3DFF0000);
 
+#if VAR_ENABLE_IMX_UART
 	if (console_base == 0U) {
 		console_base = imx8m_uart_get_base();
 	}
@@ -221,6 +223,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		IMX_CONSOLE_BAUDRATE, &console);
 	/* This console is only used for boot stage */
 	console_set_scope(&console, CONSOLE_FLAG_BOOT);
+#endif
 
 	imx8m_caam_init();
 

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -210,8 +210,9 @@ static void bl31_tzc380_setup(void)
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		u_register_t arg2, u_register_t arg3)
 {
+#if VAR_ENABLE_IMX_UART
 	unsigned int console_base = IMX_BOOT_UART_BASE;
-	static console_t console;
+#endif
 	unsigned int val;
 	unsigned int i;
 
@@ -231,6 +232,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 	val = mmio_read_32(IMX_IOMUX_GPR_BASE + 0x2c);
 	mmio_write_32(IMX_IOMUX_GPR_BASE + 0x2c, val | 0x3DFF0000);
 
+#if VAR_ENABLE_IMX_UART
 	if (console_base == 0U) {
 		console_base = imx8m_uart_get_base();
 	}
@@ -239,6 +241,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		IMX_CONSOLE_BAUDRATE, &console);
 	/* This console is only used for boot stage */
 	console_set_scope(&console, CONSOLE_FLAG_BOOT);
+#endif
 
 	imx8m_caam_init();
 

commit 7af55453e6b5b142d4e871c8eb126d0a28a0c0e2

plat: imx8m: Fix compile errors with active VAR_ENABLE_IMX_UART
Exemplary compile error for imx8mp:

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c: In function 'bl31_early_platform_setup2':
plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c:242:40: error: 'console' undeclared (first use in this function)
  242 |                 IMX_CONSOLE_BAUDRATE, &console);
      |                                        ^~~~~~~

Fixes: 1997e363065a "plat: imx8m: Disable ATF console_imx_uart_register"
Signed-off-by: Markus Heidelberg <m.heidelberg@cab.de>

plat/imx/imx8m/imx8mm/imx8mm_bl31_setup.c
@@ -189,6 +189,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 {
 #if VAR_ENABLE_IMX_UART
 	unsigned int console_base = IMX_BOOT_UART_BASE;
+	static console_t console;
 #endif
 	int i;
 

plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
@@ -185,6 +185,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 {
 #if VAR_ENABLE_IMX_UART
 	unsigned int console_base = IMX_BOOT_UART_BASE;
+	static console_t console;
 #endif
 	unsigned int val;
 	int i;

plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -212,6 +212,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 {
 #if VAR_ENABLE_IMX_UART
 	unsigned int console_base = IMX_BOOT_UART_BASE;
+	static console_t console;
 #endif
 	unsigned int val;
 	unsigned int i;